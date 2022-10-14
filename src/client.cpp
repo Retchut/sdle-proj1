57e6d6710a87a53c240764807d4112a90b45f1bc
Mário Travassos <retchut@protonmail.ch>
2022-10-14 12:39:04 +0100

    feat: implement cli argument parsing

--- src/client.cpp
+++ src/client.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 #include <zmq.hpp>
 
-int main () {
-    //  Prepare our context and socket
-    zmq::context_t context(1);
-    zmq::socket_t socket (context, zmq::socket_type::req);
-    socket.connect("tcp://127.0.0.1:5555");
-
-    int i = 0;
-    while(true){
-        zmq::message_t request(4);
-
-        memcpy(request.data(), "test", 4);
-        std::cout << "sent message " << i << std::endl;
-        socket.send (request, zmq::send_flags::none);
-
-        //Get a reply
-        zmq::message_t reply;
-
-        std::cout << "waiting for reply" << std::endl;
-        socket.recv (reply, zmq::recv_flags::none);
-        std::cout << reply.data() << " " << i << std::endl;
-        i++;
+void printUsage (){
+    std::string usage = "Usage:\n\t./client\nor\n\t./client put <topic>";
+    std::cout << usage << std::endl;
+}
+int main (int argc, char *argv[]) {
+    switch(argc){
+        case 1:
+            // run server, receiving connections
+            std::cout << "Running client eventually" << std::endl;
+            return 0;
+        case 3:
+            if(std::string(argv[1]) != "get"){
+                printUsage();
+                return 1;
+            }
+            std::cout << "getting message from topic " << std::string(argv[2]) << std::endl;
+            return 0;
+        default:
+            printUsage();
+            return 1;
     }
+
+    // int i = 0;
+    // while(true){
+    //     zmq::message_t request(4);
+
+    //     memcpy(request.data(), "test", 4);
+    //     std::cout << "sent message " << i << std::endl;
+    //     socket.send (request, zmq::send_flags::none);
+
+    //     //Get a reply
+    //     zmq::message_t reply;
+
+    //     std::cout << "waiting for reply" << std::endl;
+    //     socket.recv (reply, zmq::recv_flags::none);
+    //     std::cout << reply.data() << " " << i << std::endl;
+    //     i++;
+    // }
 
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -4,33 +4,48 @@
 
 int THREAD_NUM = 2;
 
-int main () {
-    //  Prepare our context and socket
-    zmq::context_t context (THREAD_NUM);
-    zmq::socket_t socket(context, zmq::socket_type::rep);
-    socket.bind("tcp://127.0.0.1:5555");
-
-    int i = 0;
-    while(true){
-        try{
-            zmq::message_t request;
-
-            std::cout << "waiting for message" << std::endl;
-            auto res = socket.recv (request, zmq::recv_flags::none); //check flags
-            std::cout << "Received message: " << request.data() << std::endl;
-
-            zmq::message_t reply;
-            
-            memcpy(reply.data(), "reply", 5);
-            std::cout << "replying " << i << std::endl; 
-            socket.send(reply, zmq::send_flags::none);
-            i++;
-        }
-        catch(...){
-            std::cout << "client died" << std::endl;
+void printUsage (){
+    std::string usage = "Usage:\n\t./server\nor\n\t./server put <topic>";
+    std::cout << usage << std::endl;
+}
+int main (int argc, char *argv[]) {
+    switch(argc){
+        case 1:
+            // run server, receiving connections
+            std::cout << "Running server eventually" << std::endl;
+            return 0;
+        case 3:
+            if(std::string(argv[1]) != "put"){
+                printUsage();
+                return 1;
+            }
+            std::cout << "putting message on topic " << std::string(argv[2]) << std::endl;
+            return 0;
+        default:
+            printUsage();
             return 1;
-        }
     }
+
+    // int i = 0;
+    // while(true){
+    //     try{
+    //         zmq::message_t request;
+
+    //         std::cout << "waiting for message" << std::endl;
+    //         auto res = socket.recv (request, zmq::recv_flags::none); //check flags
+    //         std::cout << "Received message: " << request.data() << std::endl;
+
+    //         zmq::message_t reply;
+            
+    //         memcpy(reply.data(), "reply", 5);
+    //         std::cout << "replying " << i << std::endl; 
+    //         socket.send(reply, zmq::send_flags::none);
+    //         i++;
+    //     }
+    //     catch(...){
+    //         std::cout << "client died" << std::endl;
+    //     }
+    // }
 
     return 0;
 }
