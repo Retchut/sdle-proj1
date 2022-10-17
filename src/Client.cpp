36f92ec32c378fd6928c30bc312befb56bb856f9
Adelaide <up201907487@fe.up.pt>
2022-10-17 11:03:44 +0100

    Add parsing for client instructions

--- src/Client.cpp
+++ src/Client.cpp
@@ -36,6 +36,13 @@ int changeLastMessageID (std::map<std::string, int> &subscribedTopics, std::stri
     return 0;
 }
 
+int getLastMessageIDFromTopic (std::map<std::string, int> &subscribedTopics, std::string topic) {
+    if(subscribedTopics.find(topic) != subscribedTopics.end()) {
+        return subscribedTopics.at(topic);
+    }
+    return -1;
+}
+
 void testClientCommunication(int clientID){
     // Testing
     zmq::context_t context(1);
@@ -83,7 +90,7 @@ void testClientCommunication(int clientID){
     // END-Testing
 }
 
-void runClient(){
+void runClient(int clientID){
 
     zmq::context_t context(1);
     zmq::socket_t socket (context, zmq::socket_type::req);
@@ -92,7 +99,69 @@ void runClient(){
     std::map<std::string, int> subscribedTopics;
 
     while (true) {
+
+        //Read from stdin
+        std::string request;
+        std::string instruction;
+        std::cout << "Instruction Type?\n 1. SUB\n 2. UNSUB\n 3. GET\n 4. PUT\n"; 
+        std::getline(std::cin, instruction);
+
+        try {
+            if (stoi(instruction) <= 4 || stoi(instruction) >= 1){
+                request += instruction; //TODO PARSE TO STRING
+            } else {
+                std::cout << "Invalid instruction\n";
+                continue;
+            }
+        }
+        catch(const std::invalid_argument& e){
+            std::cerr << "Invalid argument: " << e.what() << "\n";
+            continue;
+        }
+
+        request += " " + std::to_string(clientID) + " ";
+
+        std::string topic;
+        std::cout << "Topic Type?\n"; 
+        std::getline(std::cin, topic);
+
+        request += topic + " ";
+
+        switch (stoi(instruction)) {
+            case 1:
+                break;
+            case 2:
+                if(subscribedTopics.find(topic) == subscribedTopics.end()) {
+                    std::cerr << "Topic not subscribed: \n";
+                    continue;
+                }
+                break;
+            case 3:
+                request += std::to_string(getLastMessageIDFromTopic(subscribedTopics, topic));
+                break;
+            case 4:
+                std::string content;
+                std::cout << "Insert message:\n"; 
+                std::getline(std::cin, content);
+                request += content;
+        }
         
+        std::cout << "request: " << request << std::endl;
+
+        zmq::message_t requestMsg(request.length());
+
+        //Send
+        memcpy(requestMsg.data(), request.c_str(), request.length());
+        socket.send (requestMsg, zmq::send_flags::none);
+        std::cout << "---Sent message: " << request.c_str() << std::endl;
+
+        //Get a reply
+        zmq::message_t reply;
+        std::cout << "...Waiting for reply" << std::endl;
+        auto size = socket.recv (reply, zmq::recv_flags::none);
+        char * reply_c_str = (char *) reply.data();
+        reply_c_str[size.value()] = '\0';
+        std::cout << "---Reply: " << reply_c_str << std::endl;
     }
 }
 
@@ -112,8 +181,8 @@ int main (int argc, char *argv[]) {
             setupStorage(entityName);
 
             std::cout << "Running client " << clientID << std::endl;
-            testClientCommunication(clientID);
-            //runClient();
+            //testClientCommunication(clientID);
+            runClient(clientID);
         }
             return 0;
         default:
