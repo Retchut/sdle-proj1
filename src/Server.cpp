4198ce413ff3e77d02fbb530d6f053f442ea3898
Luis Guimaraes <up202204188@fe.up.pt>
2022-10-19 18:54:55 +0100

    change context constructor to use THREAD_NUM global variable

--- src/Server.cpp
+++ src/Server.cpp
@@ -27,7 +27,7 @@ void printUsage (){
 
 int run(std::map<std::string, Topic> * topics_map){
 
-    zmq::context_t context (3);
+    zmq::context_t context (THREAD_NUM);
     zmq::socket_t socket (context, zmq::socket_type::rep);
     socket.bind ("tcp://*:5555");
     while(true){
